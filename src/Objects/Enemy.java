dc427122d76a153e41eb4dedcff0205bf50311d8
jonDorito <jonDorito@users.noreply.github.com>
2018-10-25 08:03:09 -0600

    A library with all the JARs needed has been added to the proyect
    - ListSerializer now has methods for parsing JSON to Objects and
    viceversa
    - A folder called lib has been added with all the JARs needed to run the
    project somehow smoothly

---
+++ src/Objects/Enemy.java
@@ -0,0 +1,73 @@
+package Objects;
+
+import Objects.Player;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.*;
+
+public class Enemy{
+	private double xPosition, yPosition;
+	private boolean blowFire;
+	private double screenWidth, screenHeight;
+	
+	
+	public Enemy(double screenWidth, double screenHeight) {
+		this.screenHeight = screenHeight;
+		this.screenWidth = screenWidth;
+	}
+	
+	public void Fire() {
+		
+	}
+	
+	
+	public void MoveBy(int xPosition, int yPosition, double cx, double cy, double xLayout, double yLayout) {
+		if (xPosition == 0 && yPosition == 0) {
+			return;
+		}
+		
+		double x = cx + xLayout + xPosition;
+		double y = cy + yLayout + yPosition;
+		
+		Moveto(x, y, cx, cy);
+		
+	}
+	
+	public void Moveto (double x, double y, double cx, double cy ) {
+		System.out.println("cx " + cx + " cy " + cy + " x " + x + " y " + y);
+		if (x-cx >= 0 && x+cx <=screenWidth && y-cy >= 0 && y+cy <=screenHeight) {
+			
+			this.xPosition = x-cx;
+			this.yPosition = y-cy;
+			
+		}
+	}
+
+	public double getxPosition() {
+		return xPosition;
+	}
+
+	public void setxPosition(double xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public double getyPosition() {
+		return yPosition;
+	}
+
+	public void setyPosition(double yPosition) {
+		this.yPosition = yPosition;
+	}
+	
+	
+	
+	
+	
+	
+
+}
